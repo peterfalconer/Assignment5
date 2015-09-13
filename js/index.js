50aeb08ec14ee089478e55c48f0709530cce0608
peterfalconer <p.falconer@acfonline.org.au>
2015-09-13 21:29:37 +1000

    Functional cut

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+      var cities = ["NEW YORK","NEW YORK CITY","NY","SAN FRANCISCO","SF","BAY AREA","LOS ANGELES","LA","LAX","SYDNEY","SYD","AUSTIN","ATX"];
+      var images = ["nyc.jpg","nyc.jpg","nyc.jpg","sf.jpg","sf.jpg","sf.jpg","la.jpg", "la.jpg", "la.jpg", "sydney.jpg", "sydney.jpg","austin.jpg","austin.jpg"];
+      $("#submit-btn").click(UpdateCityImage);
+      
+
+      function UpdateCityImage(e) {
+        var cityName = "";
+        e.preventDefault();
+        cityName = $("#city-type").val();
+        if (citynameIsValid(cityName)) {
+            showCityAsBackground(cityName);
+        } else {
+            showDefaultCityBackground(cityName);
+        }
+      }
+
+      function citynameIsValid(cityName) {
+        var result = false;
+        
+        if (cities.indexOf(cityName.toUpperCase()) > -1) {
+            result = true;
+        }
+        return result;
+      }
+
+      function showCityAsBackground(cityName) {
+        var i = 0;
+        var bgURL = ""
+        i = cities.indexOf(cityName.toUpperCase());
+        bgURL = "url(images/" + images[i] + ")"
+        $("body").css("background-image", bgURL);
+      }
+
+      function showDefaultCityBackground(cityName) {
+        $("body").css("background-image","url(images/citipix_skyline.jpg)");  
+        $("#city-type").val(cityName + " - no image");
+      }
+});
